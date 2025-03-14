f69a65b380aa94e297c6076bf5f4723776d07fe4
Milena Genez <genezmilena@gmail.com>
2025-03-14 16:58:58 -0300

    Commit inicial

---
+++ src/conexiones/Conexiones.java
@@ -0,0 +1,8 @@
+
+package conexiones;
+
+public class Conexiones {
+    private static final String url = "jdbc:mysql://localhost:3306/produccion";
+    private static final String usuario = "root";
+    private static final String pass = "";
+}
